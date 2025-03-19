4555bb6a289611615d99a1c420e4b11ef2d157fe
CVL <abdullah.rasul95@gmail.com>
2025-03-19 03:54:52 -0400

    Added Moveit for the excavator

--- install/excavator_description/share/excavator_description/launch/gazebo.launch.py
+++ install/excavator_description/share/excavator_description/launch/gazebo.launch.py
@@ -10,26 +10,34 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
 
+
 def generate_launch_description():
     excavator_description = get_package_share_directory("excavator_description")
 
-    model_arg = DeclareLaunchArgument(
-        name="model",
-        default_value=os.path.join(excavator_description, "urdf", "excavator.urdf.xacro"),
-        description="Absolute path to robot urdf file"
+    model_arg = DeclareLaunchArgument(name="model", default_value=os.path.join(
+                                        excavator_description, "urdf", "excavator.urdf.xacro"
+                                        ),
+                                      description="Absolute path to robot urdf file"
     )
 
     gazebo_resource_path = SetEnvironmentVariable(
         name="GZ_SIM_RESOURCE_PATH",
         value=[
             str(Path(excavator_description).parent.resolve())
-        ]
-    )
+            ]
+        )
     
     ros_distro = os.environ["ROS_DISTRO"]
+    is_ignition = "True" if ros_distro == "humble" else "False"
 
-    robot_description = ParameterValue(Command(["xacro ", LaunchConfiguration("model")]),
-                                       value_type=str)
+    robot_description = ParameterValue(Command([
+            "xacro ",
+            LaunchConfiguration("model"),
+            " is_ignition:=",
+            is_ignition
+        ]),
+        value_type=str
+    )
 
     robot_state_publisher_node = Node(
         package="robot_state_publisher",
@@ -39,26 +47,27 @@ def generate_launch_description():
     )
 
     gazebo = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            os.path.join(get_package_share_directory("ros_gz_sim"), "launch"), "/gz_sim.launch.py"
-        ]),
-        launch_arguments=[
-            ("gz_args", [" -v 4 -r empty.sdf "])
-        ]
-    )
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory("ros_gz_sim"), "launch"), "/gz_sim.launch.py"]),
+                launch_arguments=[
+                    ("gz_args", [" -v 4 -r empty.sdf "]
+                    )
+                ]
+             )
 
     gz_spawn_entity = Node(
         package="ros_gz_sim",
         executable="create",
         output="screen",
-        arguments=["-topic", "robot_description", "-name", "excavator"]
+        arguments=["-topic", "robot_description",
+                   "-name", "excavator"],
     )
 
     gz_ros2_bridge = Node(
         package="ros_gz_bridge",
         executable="parameter_bridge",
         arguments=[
-            "/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock]",
+            "/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock",
         ]
     )
 
